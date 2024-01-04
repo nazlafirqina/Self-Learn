d826062a743a36d614a8346ee25431532468e160
Nazla Firqina <firqirahmi@gmail.com>
2024-01-04 12:22:08 +0700

    file phyton

---
+++ linear_search.py
@@ -0,0 +1,32 @@
+from unittest import result
+
+
+def linear_search(list,target):
+    """
+    Returns the index position of the taget if found, else returns None
+    """
+    for i in range (0, len(list)):
+        if list[i] == target:
+            print ("panjang list", len(list))
+            print("ini index:", i)
+            return i
+        # return None
+
+
+def verify(index):
+    if index is not None:
+        print("Target found at index:", index)
+    else:
+        print("Target not found in list")
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = linear_search(numbers,9)
+print("ini result", result)
+verify(result)
+
+
+# result = linear_search(numbers,6)
+# print("ini result", result)
+# verify(result)
